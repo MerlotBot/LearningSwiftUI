4cceb1e6e9eeaf3983969614fafb6da54f4f97b8
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-07-12 15:56:33 +0200

    BackgroundBlur

---
+++ LearningSwiftUI/BlurView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeList.swift
+//  LearningSwiftUI
+//
+//  Created by Antonakakis Nikolaos on 12.07.19.
+//  Copyright © 2019 Antonakakis Nikolaos. All rights reserved.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    
+    let style: UIBlurEffect.Style
+    
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            ])
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView,
+                      context: UIViewRepresentableContext<BlurView>) {
+        
+    }
+}
